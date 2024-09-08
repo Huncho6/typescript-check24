8fd62fb46008d50e10334ed00ab10817360980ef
teebliqs4@gmail.com <teebliqs4@gmail.com>
2024-09-08 23:10:55 +0100

    typescript

---
+++ car.ts
@@ -0,0 +1,28 @@
+interface Vehicle {
+  make: string; // The manufacturer of the vehicle (e.g., Toyota, Ford)
+  model: string; // The model of the vehicle (e.g., Corolla, Mustang)
+  year: number; // The year the vehicle was made
+
+  // The start method is a function that doesn't return any value (void) and logs a message.
+  start(): void;
+}
+
+class Car implements Vehicle {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  start(): void {
+    console.log("Car engine started");
+  }
+}
+
+const myCar = new Car("Toyota", "Corolla sprinter", 2021);
+
+myCar.start();
