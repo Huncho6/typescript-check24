25b195a90ba39d84540f87023197475f449c842f
teebliqs4@gmail.com <teebliqs4@gmail.com>
2024-09-09 10:19:16 +0100

    checkpoint react with typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Counter from "./Counter"
+import Greeting from "./Greeting"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Greeting name={"ibrahim"} />
+    <Greeting name={"Tunde"} />
+    <Counter />
+    
+    </>
+  )
+}
+
+export default App
